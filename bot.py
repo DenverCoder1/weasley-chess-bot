caf6ddf5b7092ad072107df040a3f14f8e2a7103
Jonah Lawrence <jonah@freshidea.com>
2021-05-03 06:17:59 +0300

    formatting

--- bot.py
+++ bot.py
@@ -3,28 +3,29 @@ import os
 from discord.ext import commands
 import config
 
+
 def main():
-	# allows privledged intents for monitoring members joining, roles editing, and role assignments
-	# these need to be enabled in the developer portal as well
-	intents = discord.Intents.default()
-	intents.guilds = True
-	intents.members = True
+    # allows privledged intents for monitoring members joining, roles editing, and role assignments
+    # these need to be enabled in the developer portal as well
+    intents = discord.Intents.default()
+    intents.guilds = True
+    intents.members = True
 
-	client = commands.Bot(config.BOT_PREFIX, intents=intents)  # bot command prefix
+    client = commands.Bot(config.BOT_PREFIX, intents=intents)  # bot command prefix
 
-	# Get the modules of all cogs whose directory structure is modules/<module_name>/cog.py
-	for folder in os.listdir("cogs"):
-		if os.path.exists(os.path.join("cogs", folder, "cog.py")):
-			client.load_extension(f"cogs.{folder}.cog")
+    # Get the modules of all cogs whose directory structure is modules/<module_name>/cog.py
+    for folder in os.listdir("cogs"):
+        if os.path.exists(os.path.join("cogs", folder, "cog.py")):
+            client.load_extension(f"cogs.{folder}.cog")
 
-	@client.event
-	async def on_ready():
-		"""When discord is connected"""
-		print(f"{client.user.name} has connected to Discord!")
+    @client.event
+    async def on_ready():
+        """When discord is connected"""
+        print(f"{client.user.name} has connected to Discord!")
 
-	# Run Discord bot
-	client.run(config.DISCORD_TOKEN)
+    # Run Discord bot
+    client.run(config.DISCORD_TOKEN)
 
 
 if __name__ == "__main__":
-	main()
+    main()
