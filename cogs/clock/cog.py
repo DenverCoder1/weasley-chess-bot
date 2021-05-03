7594ff9465ef86364f56664cf036305ef6780d6e
Jonah Lawrence <jonah@freshidea.com>
2021-05-03 07:23:29 +0300

    rearranged init function

--- cogs/clock/cog.py
+++ cogs/clock/cog.py
@@ -23,24 +23,24 @@ class ClockCog(commands.Cog, name="Clock"):
     @loop(seconds=CHECK_INTERVAL_SECONDS)
     async def clock(self):
         """loop to update clock"""
-        # update the clock
         try:
+            # update the clock
             await self.__message.edit(embed=clock_embed())
         except HTTPException:
+            # if message doesn't exist, create a new one
             self.__message = await get_or_create_embed(self.__bot, self.__channel)
 
     @clock.before_loop
-    async def clock_init(self):
+    async def clock_init(self) -> None:
         """print startup info before reddit feed loop begins"""
         # get clock channel object
         self.__channel = self.__bot.get_channel(config.CLOCK_CHANNEL_ID)
-        # if channel exists, get the last message from the bot
-        if isinstance(self.__channel, discord.TextChannel):
-            # find the last message from the bot in the channel or create a new one
-            self.__message = await get_or_create_embed(self.__bot, self.__channel)
-            return
-        # error
-        print("Couldn't find that channel.")
+        # check that channel exists
+        if not isinstance(self.__channel, discord.TextChannel):
+            print("Couldn't find that channel.")
+            return self.clock.cancel()
+        # if channel exists, get the last message from the bot or create one
+        self.__message = await get_or_create_embed(self.__bot, self.__channel)
 
 
 def setup(bot):
