32e170359b5373b353c5d25f5d12ea68b7d3f6ff
Jonah Lawrence <jonah@freshidea.com>
2021-05-06 03:18:43 +0300

    shorten title text

--- cogs/clock/clock.py
+++ cogs/clock/clock.py
@@ -16,7 +16,7 @@ def clock_embed() -> discord.Embed:
     now = datetime.utcnow()
     clock = get_clock_emoji(now)
     return build_embed(
-        title=f"{clock} The current time is {format_date(now)} UTC",
+        title=f"{clock} It is now {format_date(now)} UTC",
         description=f"```{text2art(now.strftime('%H:%M'))}```",
     )
 
